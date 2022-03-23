672c88d4141b7387e96faff117454ec4842d7c20
limtaechan <tccap123@naver.com>
2022-03-23 17:42:09 +0900

    back서버 진행중...

--- server.js
+++ server.js
@@ -11,12 +11,66 @@ app.use(cors({
     credentials:true,
 }))
 
-app.post('/api/board/list',(req,res)=>{
-    const response = {
-        result:[]
-    }
-    res.json(response)
-    console.log(response)
+app.post('/api/user/join',async (req,res)=>{
+    console.log(req.body)
+    const {userid,userpw,profile_img,username,nickname,address,gender,telephone,phonenumber,email,introduce,level,active,point} = req.body
+    const sql = `INSERT INTO user(
+            userid,
+            userpw,
+            profile_img,
+            username,
+            nickname,
+            address,
+            gender,
+            telephone,
+            phonenumber,
+            email,
+            introduce,
+            level,
+            active,
+            point
+        ) values(
+            ?,?,?,?,?,?,?,?,?,?,?,1,1,0
+        )`
+    const prepare = [userid,userpw,profile_img,username,nickname,address,gender,telephone,phonenumber,email,introduce,level,active,point]
+    
+    try{
+        const [result] = await pool.execute(sql,prepare) 
+        const response = {
+            result:{
+                row:result.affectedRows,
+                id:result.insertId
+            },
+            errno:0,
+        }
+        // res.setHeader('Set-cookie','name=ingoo; path=/; Domain=localhost;')
+        // res.cookie('name','',{
+        //     path:'/',
+        //     httpOnly:true,
+        //     secure:true,
+        //     domain:'localhost'
+        // })
+        res.json(response) 
+    
+        }catch(e){
+        console.log(e.message)
+        const response = {
+            result:{
+                row:0,
+                id:0
+            },
+            errno:1,
+        }
+        res.json(response)
+        }
+    
+    })
+    
+
+
+app.post('/user/login',(req,res)=>{
+    const postman = req.body
+    console.log(JSON.parse(postman))
 })
 
 app.listen(4001,()=>{
