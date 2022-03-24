3ce115dc97e36a48808d226576c0fc319ff4f4b9
limtaechan <tccap123@naver.com>
2022-03-24 14:00:38 +0900

    디렉토리 구조 정리

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,8 +3,8 @@ const { createSignature } = require('../utils/jwt')
 exports.Auth = (req,res,next) => {
     try {
         const { token } = req.cookies
-        if (token === undefined) throw new Error('토큰 오류')
-
+        console.log(`토큰있뉘?`,token)
+        if (token === undefined) throw new Error('토큰 없다')
         const [header,payload,sign] = token.split('.')    
         const signature = createSignature(header,payload)
 
