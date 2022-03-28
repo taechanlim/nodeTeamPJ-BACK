04405c9e85a9ed70c471f26c0ebc58cc2d400f75
limtaechan <tccap123@naver.com>
2022-03-28 16:41:01 +0900

    수정완료

--- routes/board/boardRouter.js
+++ routes/board/boardRouter.js
@@ -6,7 +6,7 @@ router.post('/list',boardController.list);
 router.post('/write',boardController.write);
 router.post('/view',boardController.view);
 router.post('/update',boardController.update);
-// router.post('/delete',boardController.delete);
+router.post('/delete',boardController.delete);
 
 
 module.exports = router;
