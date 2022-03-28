5c1caa5fa933f50da9bd9a55c6ccd105574d3598
limtaechan <tccap123@naver.com>
2022-03-28 11:27:00 +0900

    충돌방지

--- routes/board/boardController.js
+++ routes/board/boardController.js
@@ -3,16 +3,17 @@ const pool = require('../../Database/db.js').pool
 
 exports.list = async (req,res)=>{
     const sql = `SELECT idx,cate_name,thumbnail,subject,nickname,DATE_FORMAT(date,'%Y-%m-%d') as date,hit,likes FROM board ORDER BY idx DESC`
-    
+    const sql2 = `SELECT count(idx) as total_record FROM board`
     let response = {
         errno:1
     }
 
     try{
         const [result] = await pool.execute(sql)
-        
+        const [[{total_record}]] = await pool.execute(sql2)
         response = {
             ...response,
+            total_record,
             errno:0,
             result
         }
@@ -32,7 +33,7 @@ exports.write = async (req,res)=>{
     const nickname = JSON.parse(decodingPayload).nickname
     
     
-    const sql = `INSERT INTO board(cate_name,subject,content,nickname) values(?,?,?,?)`
+    const sql = `INSERT INTO board(cate_name,subject,content,nickname) VALUES (?,?,?,?),(1,1,1,?)`
     const prepare = [cate_name,subject,content,nickname]
     
     let response = {
@@ -60,9 +61,57 @@ exports.write = async (req,res)=>{
 }
 
 exports.view = async (req,res)=>{
-    console.log(req)
-    const {idx} = req.params
+    // console.log(req)
+    const idx = req.query
     const sql = `SELECT * FROM board WHERE idx=?`
+    const prepare = [idx]
+    let response = {
+        errno:0
+    }
+    try{
+        const [result] = await pool.execute(sql,prepare)
+        response = {
+            ...response,
+            result
+        }
+    }catch(e){
+            {
+                console.log(e.message)
+                response={
+                    errno:1
+                }
+            }
+    }
+    res.json(response)
+}
+
+// exports.getUpdate = async (req,res) => {
+    
+
+//     const sql = `select * from board where idx=${index}`
+
+//     let response = {
+//         errno:0
+//     }
+
+//     try {
+//         const [result] = await pool.execute(sql)
+//         response = {
+//             ...response,
+//             result
+//         }
+//     } catch (error) {
+//         console.log(error.message)
+//         response = {
+//             errno:1
+//         }
+//     }
+//     res.json(response)
+// };
+
+exports.postUpdate = async (req,res)=>{
+    const board = req.body
+    const sql = `UPDATE board SET subject='${board.subject}',content='${board.content}' WHERE idx=${board.idx}`
     const prepare = [idx]
     let response = {
         errno:0
