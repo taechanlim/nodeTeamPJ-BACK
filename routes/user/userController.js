5475a722df078bfc1568280d0ebd8d7230ea914f
cjh091 <cjh5701@hanmail.net>
2022-03-31 11:42:39 +0900

    내가 쓴 게시글, 내가 쓴 댓글 ,내가 스크랩한 글 보기 [수정해야됨]

--- routes/user/userController.js
+++ routes/user/userController.js
@@ -116,4 +116,70 @@ exports.login = async (req,res)=>{
     }
 }
 
+//쓴 게시글 보기 수정해야댐
+const getBoard = async (req, res) => {
+    try {
+        const {user} = req.session
+        const page = req.params.num
+        const count = 10
+        let sql = 'SELECT idx, title, DATE_FORMAT(date, "%Y-%m-%d") AS date, view, likes FROM user LEFT JOIN board ON user.userid=board.userid WHERE board.userid=?'
+        const [rows, fields] = await promisePool.query(sql, [user.userid])
+        pageNum = []
+        for (let i=0; i<rows.length/count; i++) {pageNum.push(i)}
+        const result = paging(page, count, rows)
+        res.render('./user/profile/user_board.html', {
+            result,
+            page,
+            pageNum,
+            user
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send('<h1>Internal Server Error</h1>')
+    }
+}
 
+//쓴 댓글 보기
+const getComment = async (req, res) => {
+    try {
+        const {user} = req.session
+        const page = req.params.num
+        const count = 10
+        let sql = 'SELECT cid, comment, DATE_FORMAT(c_date, "%Y-%m-%d %r") AS date, bid FROM user LEFT JOIN comment ON user.userid=comment.c_userid WHERE c_userid=?'
+        const [rows, fields] = await promisePool.query(sql, [user.userid])
+        pageNum = []
+        for (let i=0; i<rows.length/count; i++) {pageNum.push(i)}
+        const result = paging(page, count, rows)
+        res.render('./user/profile/user_comment.html', {
+            result,
+            page,
+            pageNum,
+            user
+        })
+    } catch {
+        console.log(err)
+        res.status(500).send('<h1>Internal Server Error</h1>')
+    }
+}
+//스크랩 보기
+const getScrap = async (req, res) => {
+    try {
+        const {user} = req.session
+        const page = req.params.num
+        const count = 10
+        let sql = 'SELECT s_idx, s_userid, idx, title, nickname, DATE_FORMAT(date, "%Y-%m-%d") AS date FROM scrap LEFT JOIN board ON scrap.bid=board.idx WHERE s_userid=?;'
+        const [rows, fields] = await promisePool.query(sql, [user.userid])
+        pageNum = []
+        for (let i=0; i<rows.length/count; i++) {pageNum.push(i)}
+        const result = paging(page, count, rows)
+        res.render('./user/profile/user_scrap.html', {
+            user,
+            result,
+            page,
+            pageNum
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send('<h1>Internal Server Error</h1>')
+    }    
+}
