09cd6339a56bdb39d0a1fb029184abf0b5ab598f
cjh091 <cjh5701@hanmail.net>
2022-04-01 15:24:27 +0900

    comment write 오류수정중

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,12 @@ const userRouter = require('./user/userRouter.js');
 const boardRouter = require('./board/boardRouter.js');
 // const adminRouter = require('./admin/adminRouter.js');
 // const replyRouter = require('./reply/replyRouter.js');
-
+const commentRouter = require('./comment/commentRouter.js')
 
 const router = express.Router();
 
 router.use('/api/user',userRouter);
 router.use('/api/board',boardRouter);
-
+router.use('/api/comment',commentRouter);
 router.use(Auth)
-
-
 module.exports = router;
