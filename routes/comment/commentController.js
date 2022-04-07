ef40811d6836be942635e958d363fece307d6a70
limtaechan <tccap123@naver.com>
2022-04-07 12:30:59 +0900

    댓글추천

--- routes/comment/commentController.js
+++ routes/comment/commentController.js
@@ -95,6 +95,55 @@ exports.delete = async (req, res) => {
     }
 }
 
+exports.recommend = async (req,res)=>{
+    const {comment_idx} = req.body //board idx값
+    const {token} = req.cookies
+    const [,payload,] = token.split('.')
+    const decodingPayload = Buffer.from(payload,'base64').toString()
+    const nickname = JSON.parse(decodingPayload).nickname
+    
+    const sql1 = `UPDATE comment SET recommend=recommend+1 WHERE comment_idx=${comment_idx}`
+    const sql2 = `UPDATE user SET recommend=${comment_idx} WHERE nickname='${nickname}'`
+    // const sql4 = `UPDATE board SET likes=likes+1 WHERE idx=${comment_idx}`
+    const sql3 = `select recommend from user where nickname='${nickname}'`
+    
+      
+    let response = {
+        errno:0
+    }
+    try{
+        
+        const [result] = await pool.execute(sql3)
+        console.log(result[0].recommend)
+        
+        if(result[0].recommend != comment_idx){
+            
+            const [result2] = await pool.execute(sql1)
+                              await pool.execute(sql2)
+
+            response = {
+                ...response,
+                result2
+            }
+
+            res.json(response)
+            
+        }else{
+            response = {
+                errno:1
+            }
+            res.json(response)
+        }
+
+    }catch(e){
+            {
+                console.log(e.message)
+                response={
+                    errno:1
+                }
+            }
+    }
+}
 
 
 
