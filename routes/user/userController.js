b7d4e54926b241cb485e5b3d5383e1df63110928
limtaechan <tccap123@naver.com>
2022-04-08 14:51:59 +0900

    mypage데이터 전송

--- routes/user/userController.js
+++ routes/user/userController.js
@@ -175,82 +175,57 @@ exports.welcome = async (req,res)=>{
     }
 }
 
-//쓴 게시글 보기 수정해야댐
-const getBoard = async (req, res) => {
-    try {
-        const {user} = req.session
-        const page = req.params.num
-        const count = 10
-         //sql문 수정해야됨
-        let sql = 'SELECT idx, title, DATE_FORMAT(date, "%Y-%m-%d") AS date, view, likes FROM user LEFT JOIN board ON user.userid=board.userid WHERE board.userid=?'
-        const [rows, fields] = await promisePool.query(sql, [user.userid])
-        pageNum = []
-        for (let i=0; i<rows.length/count; i++) {pageNum.push(i)}
-        const result = paging(page, count, rows)
-        res.render('./user/profile/user_board.html', {
+exports.mypageuser = async (req,res)=>{
+    const {token} = req.cookies
+    const [,payload,] = token.split('.')
+    const decodingPayload = Buffer.from(payload,'base64').toString()
+    const nickname = JSON.parse(decodingPayload).nickname
+    
+    
+    const sql = `SELECT nickname,phonenumber,address,level,point from user WHERE nickname='${nickname}'`
+    
+    try{
+        const [result] = await pool.execute(sql)
+                         
+        console.log(result)
+        const response = {
             result,
-            page,
-            pageNum,
-            user
-        })
-    } catch (err) {
-        console.log(err)
-        res.status(500).send('<h1>Internal Server Error</h1>')
+            errno:0,
+        }
+        res.json(response)
+
+    }catch(e){
+        const response = {
+            result:[],
+            errno:1,
+        }
+        res.json(response)
     }
 }
 
-//쓴 댓글 보기
-const getComment = async (req, res) => {
-    try {
-        const {user} = req.session
-        const page = req.params.num
-        const count = 10
-         //sql문 수정해야됨
-        let sql = 'SELECT cid, comment, DATE_FORMAT(c_date, "%Y-%m-%d %r") AS date, bid FROM user LEFT JOIN comment ON user.userid=comment.c_userid WHERE c_userid=?'
-        const [rows, fields] = await promisePool.query(sql, [user.userid])
-        pageNum = []
-        for (let i=0; i<rows.length/count; i++) {pageNum.push(i)}
-        const result = paging(page, count, rows)
-        res.render('./user/profile/user_comment.html', {
+exports.mypageboard = async (req,res)=>{
+    const {token} = req.cookies
+    const [,payload,] = token.split('.')
+    const decodingPayload = Buffer.from(payload,'base64').toString()
+    const nickname = JSON.parse(decodingPayload).nickname
+    
+    
+    const sql = `SELECT cate_name,subject,content,DATE_FORMAT(date,'%Y-%m-%d') as date from board WHERE nickname='${nickname}'`
+    try{
+        const [result] = await pool.execute(sql)
+                         
+        console.log(result)
+        const response = {
             result,
-            page,
-            pageNum,
-            user
-        })
-    } catch {
-        console.log(err)
-        res.status(500).send('<h1>Internal Server Error</h1>')
+            errno:0,
+        }
+        res.json(response)
+
+    }catch(e){
+        const response = {
+            result:[],
+            errno:1,
+        }
+        res.json(response)
     }
 }
-//스크랩 보기
-const getScrap = async (req, res) => {
-    try {
-        const {user} = req.session
-        const page = req.params.num
-        const count = 10
-        //sql문 수정해야됨
-        let sql = 'SELECT s_idx, s_userid, idx, title, nickname, DATE_FORMAT(date, "%Y-%m-%d") AS date FROM scrap LEFT JOIN board ON scrap.bid=board.idx WHERE s_userid=?;'
-        const [rows, fields] = await promisePool.query(sql, [user.userid])
-        pageNum = []
-        for (let i=0; i<rows.length/count; i++) {pageNum.push(i)}
-        const result = paging(page, count, rows)
-        res.render('./user/profile/user_scrap.html', { //렌더 위치 확인하고 주석지우기
-            user,
-            result,
-            page,
-            pageNum
-        })
-    } catch (err) {
-        console.log(err)
-        res.status(500).send('<h1>Internal Server Error</h1>')
-    }    
-}
-
-
-//구현해야 할 부분 스크랩보기  + 좋아요 
-// //스크랩보기 
-// exports.postScrap = async(req,res)=>{
-//     try{
-//         const {user} = req.session
-//     }
-// }
